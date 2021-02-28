32174c12d6a9bba3412143ece87e19b9823c06be
Sam Garson <samtgarson@gmail.com>
2021-02-28 14:35:37 +0000

    Blob on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 import type { NextPage } from 'next'
 import { Btn } from 'src/components/btn'
+import { BlobCanvas } from 'src/styles/components/blob-canvas'
 import { Icon } from 'src/styles/components/icon'
 import styles from '../styles/pages/home.module.scss'
 
 const Home: NextPage = () => {
   return <main className={styles.wrapper}>
-    <Icon className={styles.icon} />
-    <h1 className={styles.title}>Digest Delivery</h1>
-    <p className={styles.lede}>Get your feeds, articles and newsletters delivered to your Kindle.</p>
-    <Btn>Start reading easier</Btn>
+    <BlobCanvas className={styles.blob} />
+    <div className={styles.content}>
+      <Icon className={styles.icon} />
+      <h1 className={styles.title}>Digest Delivery</h1>
+      <p className={styles.lede}>Get your feeds, articles and newsletters delivered to your Kindle.</p>
+      <Btn>Start reading easier</Btn>
+    </div>
   </main>
 }
 
---
+++ src/styles/components/blob-canvas.tsx
@@ -0,0 +1,62 @@
+import { FC, HTMLAttributes, useEffect, useRef, useState } from 'react'
+import * as blobs2Animate from 'blobs/v2/animate'
+
+const SIZE=400
+
+const renderBlob = (ctx: CanvasRenderingContext2D) => {
+  let goAgain = true
+
+  const animation = blobs2Animate.canvasPath()
+  const renderAnimation = () => {
+    ctx.fillStyle = 'white'
+    ctx.globalAlpha = 0.3
+    ctx.clearRect(0, 0, SIZE, SIZE)
+    ctx.fill(animation.renderFrame())
+    if (goAgain) requestAnimationFrame(renderAnimation)
+  }
+  requestAnimationFrame(renderAnimation)
+
+  const generateBlob = (duration = 3000): blobs2Animate.CanvasKeyframe => ({
+    duration,
+    timingFunction: "ease",
+    callback: loopAnimation,
+    blobOptions: {
+      randomness: 5,
+      extraPoints: 1,
+      seed: Math.random(),
+      size: SIZE
+    }
+  })
+
+  const loopAnimation = () => {
+      animation.transition(generateBlob())
+  }
+
+  animation.transition(generateBlob(0))
+
+  return () => { goAgain = false }
+}
+
+export const BlobCanvas: FC<HTMLAttributes<HTMLCanvasElement>> = ({ className }) => {
+  const [render, setRender] = useState(false)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    setRender(true)
+  })
+
+  useEffect(() => {
+    if (!render) return
+
+    if (!canvasRef.current) return
+
+    const canvas = canvasRef.current.getContext("2d")
+    if (!canvas) return
+
+    return renderBlob(canvas)
+  }, [render])
+
+  if (!render) return null
+
+  return <canvas width={SIZE} height={SIZE} className={className} ref={canvasRef} />
+}
