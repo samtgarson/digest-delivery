9cee1f736f64edf9e7fd7e2301675ee3b2d16791
Sam Garson <samtgarson@gmail.com>
2021-03-21 10:52:49 +0000

    Fix styling on article items

--- src/components/list/article-item.tsx
+++ src/components/list/article-item.tsx
@@ -12,16 +12,14 @@ type ArticleItemProps = {
 }
 
 export const ArticleItem: FC<ArticleItemProps> = ({ data: { title, author, source, original_url } }) => {
-  return <li className="mb-5 flex">
-    <div className="mr-auto">
-      <h4 className={ cn("font-bold text-lg", styles.countable) }>{ decode(title) }</h4>
-      { author
-        ? <p>{ decode(author) } { source && <span>({ source })</span> }</p>
-        : source && <p>{ source }</p>
-      }
-    </div>
+  return <li className="mb-5 relative">
+    <h4 className={ cn("font-bold text-lg", styles.countable) }>{ decode(title) }</h4>
+    { author
+      ? <p>{ decode(author) } { source && <span>({ source })</span> }</p>
+      : source && <p>{ source }</p>
+    }
     { original_url && <Link passHref href={original_url}>
-      <Anchor target="_blank" naked small className="px-3">
+      <Anchor target="_blank" naked small className="px-3 absolute right-0 top-6 transform -translate-y-2/4 h-12 bg-green">
         <LinkIcon />
         <span className="ml-2 overflow-hidden w-0 link-hover:w-36 transition-all whitespace-nowrap">Read original article</span>
       </Anchor>
