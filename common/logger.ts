ef0a6ebeda749ae17bb463a531783add4ef8948b
Sam Garson <samtgarson@gmail.com>
2021-10-10 21:06:24 +0100

    Add logging to next-auth

--- common/logger.ts
+++ common/logger.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
 const debug = process.env.DEBUG
-export const log = (...args: string[]) => { debug && console.log(...args) }
+export const log = (...args: unknown[]) => { debug && console.log(...args) }
 export const errorLog = (...args: unknown[]) => { debug && console.error(...args) }
+export const warnLog = (...args: unknown[]) => { debug && console.warn(...args) }
 
 export const withPrefix = (prefix: string) => ({
-	log: (...args: string[]) => log(`[${prefix}] `, ...args),
-	error: (...args: string[]) => errorLog(`[${prefix}] `, ...args)
+	log: (...args: unknown[]) => log(`[${prefix}] `, ...args),
+	error: (...args: unknown[]) => errorLog(`[${prefix}] `, ...args),
+	warn: (...args: unknown[]) => warnLog(`[${prefix}] `, ...args)
 })
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,13 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+import { withPrefix } from "common/logger"
 import { Mailer } from "common/mailer"
 import NextAuth from "next-auth"
 import EmailProvider from 'next-auth/providers/email'
 
 const mailer = new Mailer()
 const prisma = new PrismaClient()
+const logger = withPrefix('next-auth')
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -28,6 +30,18 @@ export default NextAuth({
     async session ({ session, token: { userId } }) {
       if (userId) session.userId = `${userId}`
       return session
+    }
+  },
+  pages: {
+    error: '/login'
+  },
+  debug: !!process.env.DEBUG,
+  logger: {
+    error (code, metadata) {
+      logger.error(code, metadata)
+    },
+    warn (code) {
+      logger.warn(code)
     }
   }
 })
