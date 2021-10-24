b16fa674fb443a630a345e81753b9b22e82cd07a
Sam Garson <samtgarson@gmail.com>
2021-10-25 00:30:25 +0100

    Add storybook config

---
+++ design-system/.storybook/main.js
@@ -0,0 +1,21 @@
+module.exports = {
+  stories: ['../docs/**/*.mdx', '../docs/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss')
+        }
+      }
+    }
+  ],
+  core: {
+    builder: 'webpack5'
+  },
+  features: {
+    previewCsfV3: true
+  }
+}
---
+++ design-system/.storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Digest Delivery',
+  brandUrl: 'https://digest.delivery'
+  // brandImage: 'https://place-hold.it/350x150',
+})
